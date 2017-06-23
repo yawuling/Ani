f3bd4b46970f5728b2bd9544f38b3822c8913c43
yawuling <1297640072@qq.com>
2017-06-24 01:51:47 +0800

    首页，标签页，分类页完成，文章页markdown样式有问题

---
+++ source/js/Ani.js
@@ -0,0 +1,21 @@
+$('.js-transition-hover').hover(function(){
+  $(this).find('.js-transition-bottom').addClass('focus');
+}, function(){
+  $(this).find('.js-transition-bottom').removeClass('focus');
+});
+$('.search-input').focus(function(){
+  $(this).parent().find('.js-transition-bottom').addClass('focus');
+});
+$('.search-input').blur(function(){
+  $(this).parent().find('.js-transition-bottom').removeClass('focus');
+});
+$('#to-top').click(function(){
+  $('html, body').animate({scrollTop:0}, 'slow');
+});
+$(window).scroll(function(){
+  if ($(window).scrollTop() <= 20) {
+    $('#to-top').hide();
+  } else {
+    $('#to-top').show();
+  }
+});
